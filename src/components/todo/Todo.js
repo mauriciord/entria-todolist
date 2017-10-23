f24d455033fccc03ec3074766be2c17a2340add1
Mauricio Duarte <mauricio@st-fin-carlosal.thomasicamargo.local>
2017-10-23 18:25:01 -0200

    todo text

---
+++ src/components/todo/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todo = ({ todo, onClick }) => (
+  <div
+    onClick={onClick}
+    key={todo.id}
+    style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+    >
+      {todo.text}
+  </div>
+)
+
+export default Todo
