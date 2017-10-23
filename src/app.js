9afb49e6519d93e229131630a5350b700ab6a2bb
Mauricio Duarte <mauricio@st-fin-carlosal.thomasicamargo.local>
2017-10-23 14:43:59 -0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+
+const App = () => (
+  <div>
+    <div>Todo form</div>
+    <div>Sort Filter</div>
+    <div>TodoList</div>
+    <div>Done/Undone Filter</div>
+  </div>
+)
+
+export default App
