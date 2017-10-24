b31d36e46ebdcd7bf2f652d625307c1fc1895916
Mauricio Duarte <mauriciotem@gmail.com>
2017-10-24 15:49:05 -0200

    combineReducers

--- src/redux-flow/reducers/index.js
+++ src/redux-flow/reducers/index.js
@@ -3,8 +3,10 @@
 import { combineReducers } from 'redux'
 import todos from './todo/index'
 import filters from './filter/index'
+import sorters from './sorter/index'
 
 export default combineReducers({
   todos,
-  filters
+  filters,
+  sorters
 })
